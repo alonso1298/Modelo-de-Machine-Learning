8c9abe0af86e2f6d29ad2be11d9d2614eb60759c
alonso1298 <alonsosagrerogranados@gmail.com>
2024-10-15 20:44:04 -0600

    Se inicia con la introduccion a pandas

---
+++ introduccion_pandas.py
@@ -0,0 +1,36 @@
+# Importamos Pandas
+# Creamos una serie de números y hallamos su media
+# Hallamos la suma de dichos números
+# Creamos una SERIE de tres colores diferentes
+# Visualizamos la serie creada
+# Creamos una serie con tipos de autos, y la visualizamos
+# Combinamos las series de tipos de autos y colores en un DATAFRAME
+# Conectamos el cuaderno actual con nuestro Drive
+# Importar "ventas-autos.csv" y convertirlo en un nuevo DATAFRAME
+#Este será nuestro "Dataframe de Flujo Vehicular"
+# Exportar el Dataframe como un archivo CSV a mi carpeta "/content/drive/MyDrive/Colab Notebooks/pruebas/"
+# Analicemos los tipos de datos disponibles en el dataset de ventas autos
+# Apliquemos estadística descriptiva (cantidad de valores, media, desviación estándar, valores mínimos y máximos, cuartiles) al dataset
+# Obtenemos información del dataset utilizando info()
+# Listamos los nombres de las columnas de nuestro dataset
+# Averiguamos el "largo" de nuestro dataset
+# Mostramos las primeras 5 filas del dataset
+# Mostramos las primeras 7 filas del dataset
+# Mostramos las últimas 5 filas del dataset
+# Utilizamos .loc para seleccionar la fila de índice 3 del DataFrame
+# Utilizamos .iloc para seleccionar las filas 3, 7 y 9
+#En la documentación podrás observar la diferencia entre el funcionamiento de .loc e .iloc.
+# Seleccionar la columna "Kilometraje"
+#15/10/24, 8:42 p.m. Python TOTAL - Pandas.ipynb - Colab
+#https://colab.research.google.com/drive/1-E33EMCehgPnmqgwm13-SZSnVYOHMpuQ?usp=sharing#printMode=true 1/2
+# Encontrar el valor medio de la columnas "Kilometraje"
+# Seleccionar aquellas columnas que tengan valores superiores a 100,000 kilómetros en la columna Kilometraje
+# Creamos una tabla cruzada de doble entrada entre Fabricante y cantidad de puertas
+# Agrupamos las columnas por fabricante y buscandos el valor medio de las columnas numéricas
+# Importamos Matplotlib y creamos un gráfico con los valores de la columna Kilometraje
+# Puede que un gráfico más apropiado en este caso sea un histograma?
+# Intentamos graficar la columna de precios
+#No funcionó, verdad? Alguna idea de por qué esto puede ocurrir?
+#Una pista es buscar: "cómo convertir strings de Pandas a números"
+#Aqui hay un enlace a StackOverflow referido a este tema.
+# Elimina la puntuación de la columna de precios
